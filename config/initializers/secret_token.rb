77e88379ce18e47a6c2d8b8ef089a77b2e82c5d2
akiii <akiii@sfc.wide.ad.jp>
2012-06-08 02:15:20 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfcApi::Application.config.secret_token = '2ba78e4e14403cbea089e3f23efbd49ce9b0c6bb0bf1712bf7d3ff14fc824bda44acb8b0c58e2f40f297bf9bccfad8c83f971959e6878d5cd37e256e18fe17b4'
